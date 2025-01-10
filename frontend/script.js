a6590ce8a782e2ebf29cd3dcb76b77fdf6f53bc5
adehenry <adefemiadelaja@gmail.com>
2025-01-10 13:09:28 +0100

    week one code

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,14 @@
 // helpful link for converting image to base64: https://elmah.io/tools/base64-image-encoder/
 async function apiFetch(url) {
   const response = await fetch(url);
+
   const data = await response.json();
   return data;
 }
 
 const getData = async () => {
-  const data = await apiFetch("https://web-services-cse341.onrender.com/");
-  console.log(data);
-  // displayAllData(data);
+  const data = await apiFetch("http://localhost:3000");
+  displayAllData(data[0]);
 };
 
 function displayAllData(data) {
